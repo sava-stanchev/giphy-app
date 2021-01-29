0fa93b65d502eb486fa02b720ef7a04c6ccd4fed
Sava Stanchev <s.stanchev94@gmail.com>
2021-01-29 20:42:16 +0200

    constants and index changes

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,1 +1,11 @@
+export const HOME = 'home';
+
+export const FAVORITES = 'favorites';
+
+export const ABOUT = 'about';
+
 export const CONTAINER_SELECTOR = '#container';
+
+export const FULL_HEART = '❤';
+
+export const EMPTY_HEART = '♡';
