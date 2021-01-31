d09c18bcfa6b4df603d01e496a5a01b4430667b2
Sava Stanchev <s.stanchev94@gmail.com>
2021-01-31 16:33:37 +0200

    search-view fix for better display

--- src/view/search-view.js
+++ src/view/search-view.js
@@ -4,7 +4,9 @@ export const toSearchView = (GIFs, searchTerm) => `
   <div id="GIFs">
     <h1>GIFs found for "${searchTerm}":</h1>
     <div class="content">
-      ${GIFs.data.map(toGiphySimple).join('\n')}
+      <div class="content-inner">
+        ${GIFs.data.map(toGiphySimple).join('\n')}
+      </div>
     </div>
   </div>
 `;
