e28c889c654b92ec3222c1547b4603053f19ac36
Sava Stanchev <s.stanchev94@gmail.com>
2021-02-02 09:08:55 +0200

    more favorites funcs

--- src/view/favorites-view.js
+++ src/view/favorites-view.js
@@ -5,7 +5,7 @@ export const toFavoritesView = (GIFs) => `
   <h1>Favorite GIFs:</h1>
   <div class="content">
     <div class="content-inner">
-        ${GIFs.data.map(toGiphySimple).join('\n') || "<p>No favorites. Here's a random GIF for you.</p>"}
+        ${GIFs.map(toGiphySimple).join('\n') || "<p>No favorites. Here's a random GIF for you.</p>"}
     </div>
   </div>
 </div>
