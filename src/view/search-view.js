a3315e66dd6baf4decfb8f2c51812d7519116129
Sava Stanchev <s.stanchev94@gmail.com>
2021-02-04 09:34:49 +0200

    documentation

--- src/view/search-view.js
+++ src/view/search-view.js
@@ -1,5 +1,13 @@
 import { toGiphySimple } from './giphies-view.js';
 
+/**
+ * Provides an HTML template for the found GIFs to be displayed in.
+ * 
+ * @author Sava Stanchev
+ * @param {object} gifs - An object containing the data needed.
+ * @param {string} searchTerm - Text written in the input field.
+ * @return {string} - The HTML template, in which the GIFs are rendered.
+ */
 export const toSearchView = (gifs, searchTerm) => `
   <div id="GIFs">
     <h1>GIFs found for "${searchTerm}":</h1>
