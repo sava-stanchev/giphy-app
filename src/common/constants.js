eb4d4b5338318cfccf42d66af2aa0f72409ec6d6
Sava Stanchev <s.stanchev94@gmail.com>
2022-02-05 17:59:57 +0200

    small fixes, remove stuff that's not used

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,27 +1,21 @@
-export const API_KEY = 'n5zxeZCOVFKxLKHtZJihFzK4vpLvq5XJ';
+export const API_KEY = "n5zxeZCOVFKxLKHtZJihFzK4vpLvq5XJ";
 
-export const HOME = 'home';
+export const HOME = "home";
 
-export const TRENDING = 'trending';
+export const TRENDING = "trending";
 
-export const FAVORITES = 'favorites';
+export const FAVORITES = "favorites";
 
-export const UPLOADED = 'uploaded';
+export const UPLOADED = "uploaded";
 
-export const UPLOAD = 'upload';
+export const UPLOAD = "upload";
 
-export const ABOUT = 'about';
+export const ABOUT = "about";
 
-export const CONTAINER_SELECTOR = '#container';
+export const CONTAINER_SELECTOR = "#container";
 
-export const FULL_HEART = '❤';
+export const FULL_HEART = "❤";
 
-export const EMPTY_HEART = '♡';
-
-export const q = (selector) => document.querySelector(selector);
-
-export const qs = (selector) => document.querySelectorAll(selector);
+export const EMPTY_HEART = "♡";
 
 export const API_URL = `https://api.giphy.com/v1/gifs`;
-
-
